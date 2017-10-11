78ba593ecb51a4d97aea4eb6445f6f3df6874474
Julianne Kim <juliannne@gmail.com>
2017-10-11 18:12:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,9 +54,8 @@ class TicTacToe
       if (@board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X") ||
         (@board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O")
         return combo
-      else
-        return false
-      end
+      end 
     end
+    return false
   end
 end
