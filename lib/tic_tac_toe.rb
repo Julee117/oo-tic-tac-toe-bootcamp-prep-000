974025eb95461f309c7c15fb0e0e436f2a409295
Julianne Kim <juliannne@gmail.com>
2017-10-11 18:07:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,4 +48,14 @@ class TicTacToe
   def current_player
     turn_count.even? ? "X" : "O"
   end
+
+  def won? 
+    if WIN_COMBINATIONS.any? do |combo| 
+      @board[combo[0]] == "X" && @board[combo[1]] == "X" && board[combo[2]] == "X" ||
+      @board[combo[0]] == "O" && @board[combo[1]] == "O" && board[combo[2]] == "O"
+        return combo 
+    else 
+      return false 
+    end 
+  end
 end
